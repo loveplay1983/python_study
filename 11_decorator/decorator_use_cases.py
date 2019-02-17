8be0baa303f8dae2c206a1a43fc3eec16f61d6bb
loveplay1983 <michaelxuan@hotmail.com>
2019-02-17 16:51:09 +0800

    updating decorator use cases...

--- 11_decorator/decorator_use_cases.py
+++ 11_decorator/decorator_use_cases.py
@@ -3,3 +3,21 @@ def arg_check(f):
     def test(x):
         if type(x) == int and x > 0:
             return f(x)
+        else:
+            raise Exception('Input is not Integer.')
+    return test
+
+
+@arg_check
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+for i in range(1, 10):
+    print(i, '- ', factorial(i))
+
+
+print(factorial(-1))
+
