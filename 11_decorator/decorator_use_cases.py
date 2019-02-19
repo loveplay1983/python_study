ed2db55dcbeab3cac74857ab72a486aca5315cf0
loveplay1983 <michaelxuan@hotmail.com>
2019-02-19 19:57:44 +0800

    update decoration

--- 11_decorator/decorator_use_cases.py
+++ 11_decorator/decorator_use_cases.py
@@ -1,3 +1,9 @@
+############# helper function####################
+def nl():
+    print('\n')
+
+##################################################
+
 # Checking args with decorator
 def arg_check(f):
     def test(x):
@@ -19,5 +25,91 @@ for i in range(1, 10):
     print(i, '- ', factorial(i))
 
 
-print(factorial(-1))
+# print(factorial(-1))
+
+nl()
+
+# Couting function calls with decorators
+def call_counter(func):
+    def helper(x):
+        helper.calls += 1
+        return func(x)
+    helper.calls = 0
+    return helper
+
+@call_counter
+def my_func(x):
+    return x + 1
+
+#print(my_func.calls)
+
+#for i in range(10):
+#    my_func(i)
+
+#print(my_func.calls)
+
+
+# With arbitrary args
+def call_counter(func):
+    def helper(*args, **kwargs):
+        helper.calls += 1
+        return func(*args, **kwargs)
+    helper.calls = 0
+    return helper
+
+@call_counter
+def my_func(x):
+    return x + 1
+
+@call_counter
+def my_func2(x, y=1):
+    return x*y + 1
+
+print('my_func has been called ' + str(my_func.calls) + ' times')
+
+for i in range(10):
+    my_func(i)
+
+my_func2(1,2)
+my_func2(1,2)
+my_func2(1,2)
+print('my_func has been called ' + str(my_func.calls) + ' times')
+print('my_func2 has been called ' + str(my_func2.calls) + ' times')
+
+nl()
+
+# Decorator with parameters
+def evening_greeting(func):
+    def function_wrapper(x):
+        print('Good evening, ' + func.__name__ + ' returns')
+        func(x)
+    return function_wrapper
+
+def morning_greeting(func):
+    def function_wrapper(x):
+        print('Morning ' + func.__name_+ ' returns')
+    return function_wrapper
+
+@evening_greeting
+def foo(x):
+    print(42)
+
+foo('Hi')
+
+# The above code is "hard-coded" in which the "greeting" content cannot
+# be changed forever where we need to input arbitrary content
+
+def greeting(content):                 # The decorator now has a parameter
+    def greeting_dec(func):            # Add another layer outside the wrapper
+        def func_wrapper(x):           # Wrapper function
+            print(content + ', ' + func.__name__ + ' returns')
+            func(x)                    # The function which will be decorated (it runs itself method)
+        return func_wrapper
+    return greeting_dec
+
+@greeting('Hail')
+def foo(x):
+    print(x + ' function foo prints this line')
+
+foo('Hello world')
 
---
+++ test/class_decorator.py
@@ -0,0 +1,61 @@
+# https://www.cnblogs.com/cicaday/p/python-decorator.html
+# http://blog.51cto.com/10836356/2112490
+
+# __call__ callable object
+class Test():
+    def __call__(self):
+       #print('It is callable.')
+        return
+
+t = Test()
+t()
+
+
+# class decorator
+"""
+1. Define __init__() for a function
+2. Overload __call__() and return a function
+"""
+
+class Logging():
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        print('[DEBUG - class decorator]: enter function {func}()'.format(func=self.func.__name__))
+        return self.func(*args, **kwargs)
+
+@Logging
+def say(sth):
+    print('say{}!'.format(sth))
+
+
+
+say(' Hello World!!!')
+
+
+# class decorator with parameter
+# To make a class decorator with parameter is slightly complicated than pure class decorator in which the constructor is no longer for receive the input function, instead it takes the input parameters for the decorator and the __call__ magic method is used to `take the input function and return the function`.
+
+class Logging():
+    def __init__(self, level='INFO'):
+        self.level = level
+
+    def __call__(self, func):                           # Accept input function
+        def wrapper(*args, **kwargs):
+            print('[{level}]: enter function {func}()'.format(
+                level=self.level,
+                func=func.__name__
+                ))
+            func(*args, **kwargs)
+        return wrapper
+
+@Logging(level='INFO')
+def say(sth):
+    print('say {}!'.format(sth))
+
+say('class decorator with parameters')
+
+# Built-in decorator
+# Built-in decorator returns class object instead of function
+
